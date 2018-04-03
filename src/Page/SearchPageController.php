a3682c951b90319ed27cd9639df020895800d2b3
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-04-03 22:23:20 +0700

    FIX: Pagination

--- src/Page/SearchPageController.php
+++ src/Page/SearchPageController.php
@@ -7,21 +7,48 @@
  */
 namespace Suilven\FreeTextSearch\Page;
 
+use SilverStripe\ORM\PaginatedList;
 use SilverStripe\View\ArrayData;
 
 class SearchPageController extends \PageController
 {
     private static $allowed_actions = ['index'];
 
+    private static $db = [
+        'PageSize' => 'Int'
+    ];
+
+    private static $defaults = [
+        'PageSize' => 10
+    ];
+
     public function index()
     {
-        $searcher = new \Suilven\SphinxSearch\Service\Searcher();
 
         // @todo search indexes addition
         $q = $this->getRequest()->getVar('q');
 
-        $results = $searcher->search($q);
-        $results['Query'] = $q;
+        $result = [];
+        if (!empty($q)) {
+            $searcher = new \Suilven\SphinxSearch\Service\Searcher();
+            if ($this->PageSize == 0) {
+                $this->PageSize=10;
+            }
+            $searcher->setPageSize($this->PageSize);
+            $start = $this->getRequest()->getVar('start');
+
+            error_log('START:' . $start);
+
+            // page 1 is the first page
+            $page = empty($start) ? 1 : ($start/$this->PageSize) + 1;
+            $searcher->setPage($page);
+            $results = $searcher->search($q);
+            $results['Query'] = $q;
+        }
+
+
+
+
 
         return $results;
     }
