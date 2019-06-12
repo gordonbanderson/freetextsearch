96343e9deea6c010c0bedb695968d03ff8c00bd9
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-06-12 22:10:41 +0100

    FIX: Ensure search works

--- src/Page/SearchPageController.php
+++ src/Page/SearchPageController.php
@@ -8,6 +8,7 @@
 namespace Suilven\FreeTextSearch\Page;
 
 use SilverStripe\ORM\ArrayList;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\PaginatedList;
 use SilverStripe\View\ArrayData;
 use Suilven\SphinxSearch\Service\Suggester;
@@ -33,22 +34,26 @@ class SearchPageController extends \PageController
         /** @var array $selected */
         $selected = $this->getRequest()->getVars();
 
+        $model = SearchPage::get_by_id('Suilven\FreeTextSearch\Page\SearchPage', $this->ID);
+
+
         unset($selected['start']);
         $results = [];
 
         if (!empty($selected)) {
             $searcher = new \Suilven\SphinxSearch\Service\Searcher();
             $searcher->setFilters($selected);
-            $searcher->setIndex('flickr');
-            $searcher->setFacettedTokens(['shutterspeed', 'iso', 'aperture']);
+            $searcher->setIndex($model->IndexToSearch);
+
+            // @todo Add facetted token in a non hardwired way
+            //$searcher->setFacettedTokens(['shutterspeed', 'iso', 'aperture']);
 
             if ($this->PageSize == 0) {
-                $this->PageSize=10;
+                $this->PageSize=15;
             }
             $searcher->setPageSize($this->PageSize);
             $start = $this->getRequest()->getVar('start');
 
-            error_log('START:' . $start);
 
             // page 1 is the first page
             $page = empty($start) ? 1 : ($start/$this->PageSize) + 1;
@@ -60,28 +65,24 @@ class SearchPageController extends \PageController
         if ($results['ResultsFound'] == 0) {
             // get suggestions
             $suggester = new Suggester();
-            $suggester->setIndex('flickr');
+            $suggester->setIndex($model->IndexToSearch);
+            //$suggester->setIndex('flickr');
             $suggestions = $suggester->suggest($q);
 
             $forTemplate = [];
             foreach($suggestions as $suggestion) {
                 $forTemplate[] = ['Suggestion' => $suggestion];
             }
-            $results['Suggestions'] = new ArrayList($forTemplate);
+            //$results['Suggestions'] = new ArrayList($forTemplate);
 
             // @todo FIX - only one result returned for now
 
         }
 
 
-
-
+        // @todo Make this generic
         $results['ShowResult'] = 'FlickrResult';
         $results['CleanedLink'] = $this->Link();
         return $results;
-
-        // cannot get this override to work :(  Layout does not yield to parent Page template
-        // $data = new ArrayData($results);
-        // return $data->renderWith(['Layout/FlickrSearch', '\Page']);
     }
 }
