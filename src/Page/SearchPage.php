5d8e5f460c45b1a3cbea8a82aeca15733921c4fe
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-06-12 22:10:17 +0100

    FIX: Make the index to search configurable

--- src/Page/SearchPage.php
+++ src/Page/SearchPage.php
@@ -7,6 +7,9 @@
  */
 namespace Suilven\FreeTextSearch\Page;
 
+use SilverStripe\Forms\DropdownField;
+use Suilven\FreeTextSearch\Indexes;
+
 class SearchPage extends \Page
 {
     private static $table_name = 'SearchPage';
@@ -20,12 +23,32 @@ class SearchPage extends \Page
 
         // a permanent filter for this page, not user selectable.  Use case here is to restrict to likes of searching
         // within a specific blog only
-        'PermanentFilters' => 'Varchar(255)',
+        'IndexToSearch' => 'Varchar(255)',
 
         // page size
         'PageSize' => 'Int'
 
     ];
+
+    private static $defaults = [
+        'IndexToSearch' => 'sitetree'
+    ];
+
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $indexesService = new Indexes();
+        $indexes = $indexesService->getIndexes();
+        $indexNames = [];
+        foreach($indexes as $index) {
+            $indexNames[$index->getName()] = $index->getName();
+        }
+
+        $fields->addFieldToTab('Root.Index', DropdownField::create('IndexToSearch','Index to Search',
+            $indexNames));
+        return $fields;
+    }
 
 
     /**
