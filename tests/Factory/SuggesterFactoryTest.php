d57f960b6e99f54abc4c98cfcdd96c985944cb00
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-05-19 00:15:15 +0100

    WIP: First stab at a test for the suggester factory

---
+++ tests/Factory/SuggesterFactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Suilven\FreeTextSearch\Tests\Factory;
+
+use SilverStripe\Dev\SapphireTest;
+use Suilven\FreeTextSearch\Factory\SuggesterFactory;
+use Suilven\FreeTextSearch\Index;
+
+class SuggesterFactoryTest extends SapphireTest
+{
+    public function testSuggesterFactory()
+    {
+        $factory = new SuggesterFactory();
+        $instance = $factory->getSuggester();
+        $this->assertInstanceOf('Suilven\ManticoreSearch\Service\Suggester');
+    }
+}
