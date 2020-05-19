d12d12efb6d2820034398eff7f928f95d8901b22
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-05-19 23:35:31 +0100

    ENHANCEMENT: Test for flickr index fields

--- tests/IndexesTest.php
+++ tests/IndexesTest.php
@@ -12,6 +12,13 @@ class IndexesTest extends SapphireTest
     {
         $indexes = new Indexes();
         $actualIndexes = $indexes->getIndexes();
-        error_log(print_r($actualIndexes, 1));
+
+        /** @var Index $flickrIndex */
+        $flickrIndex = $actualIndexes[2];
+        $this->assertEquals(['Title', 'Description'], $flickrIndex->getFields());
+        $this->assertEquals(['Aperture', 'ShutterSpeed', 'ISO'], $flickrIndex->getTokens());
+        $this->assertEquals(['Suilven\ManticoreSearch\Tests\Models\FlickrTag'], $flickrIndex->getHasManyFields());
+        $this->assertEquals(['Suilven\ManticoreSearch\Tests\Models\FlickrAuthor'], $flickrIndex->getHasOneFields());
+        $this->assertEquals('flickrphotos', $flickrIndex->getName());
     }
 }
