6b880af58d1d2d433971d5d59417affce28afa02
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-08-08 09:44:29 +0100

    WIP: Further coverage

---
+++ tests/Implementation/IndexableMutatorPayloadTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace Suilven\FreeTextSearch\Tests\Factory;
+
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Dev\SapphireTest;
+use Suilven\FreeTextSearch\Factory\SuggesterFactory;
+use Suilven\FreeTextSearch\Implementation\IdentityIndexablePayloadMutator;
+
+class IndexableMutatorPayloadTest extends SapphireTest
+{
+    protected static $fixture_file = ['tests/fixtures/sitetree.yml'];
+
+    public function testMutator()
+    {
+        $page = SiteTree::get()->first();
+        error_log($page->Title);
+
+        $payload = ['Title' => $page->Title];
+        $mutator = new IdentityIndexablePayloadMutator();
+        $mutator->mutatePayload($page, $payload);
+        $this->assertEquals([
+            'Title' => 'The Break In San Marino Is Bright',
+            'Link' => '/the-break-in-san-marino-is-bright/'
+        ], $payload);
+    }
+}
