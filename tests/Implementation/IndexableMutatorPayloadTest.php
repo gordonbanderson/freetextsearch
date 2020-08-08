69eeadc60e3af0f805e1859eed490cc87e387fd3
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-08-08 10:13:48 +0100

    MINOR: Coding standards

--- tests/Implementation/IndexableMutatorPayloadTest.php
+++ tests/Implementation/IndexableMutatorPayloadTest.php
@@ -4,24 +4,23 @@ namespace Suilven\FreeTextSearch\Tests\Factory;
 
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Dev\SapphireTest;
-use Suilven\FreeTextSearch\Factory\SuggesterFactory;
 use Suilven\FreeTextSearch\Implementation\IdentityIndexablePayloadMutator;
 
 class IndexableMutatorPayloadTest extends SapphireTest
 {
     protected static $fixture_file = ['tests/fixtures/sitetree.yml'];
 
-    public function testMutator()
+    public function testMutator(): void
     {
         $page = SiteTree::get()->first();
-        error_log($page->Title);
+        \error_log($page->Title);
 
         $payload = ['Title' => $page->Title];
         $mutator = new IdentityIndexablePayloadMutator();
         $mutator->mutatePayload($page, $payload);
         $this->assertEquals([
             'Title' => 'The Break In San Marino Is Bright',
-            'Link' => '/the-break-in-san-marino-is-bright/'
+            'Link' => '/the-break-in-san-marino-is-bright/',
         ], $payload);
     }
 }
