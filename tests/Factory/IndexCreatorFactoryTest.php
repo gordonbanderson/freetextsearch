fb3806bca67cdd4248d74eee4f1a987611d689e5
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-08-20 13:13:45 +0100

    Fix index creation (#25)
    * FIX: Added metadescription as it is a text field
    
    * ENHANCEMENT: Move useful communal code from manticore search module to here
    
    * FIX: Record the index name for testing purposes
    
    * ENHANCEMENT: Tests for extracting specs of the indexable fields
    
    * MINOR: Removed todo

--- tests/Factory/IndexCreatorFactoryTest.php
+++ tests/Factory/IndexCreatorFactoryTest.php
@@ -13,6 +13,8 @@ class IndexCreatorFactoryTest extends SapphireTest
     public function testFactory(): void
     {
         $factory = new IndexCreatorFactory();
+
+        /** @var \Suilven\FreeTextSearch\Tests\Mock\IndexCreator $indexCreator */
         $indexCreator = $factory->getIndexCreator();
         $this->assertInstanceOf('Suilven\FreeTextSearch\Interfaces\IndexCreator', $indexCreator);
         $this->assertInstanceOf('Suilven\FreeTextSearch\Tests\Mock\IndexCreator', $indexCreator);
@@ -20,5 +22,37 @@ class IndexCreatorFactoryTest extends SapphireTest
         $indexCreator->createIndex('sitetree');
 
         $this->assertEquals('sitetree', IndexCreator::getIndexName());
+
+        $reflection = new \ReflectionClass(\get_class($indexCreator));
+        $method = $reflection->getMethod('getFieldSpecs');
+        $method->setAccessible(true);
+
+        $specs = $method->invokeArgs($indexCreator, ['sitetree']);
+        $this->assertEquals([
+            'Title' => 'Varchar',
+            'Content' => 'HTMLText',
+            'ParentID' => 'ForeignKey',
+            'MenuTitle' => 'Varchar',
+            'Sort' => 'Int',
+            'Created' => 'DBDatetime',
+            'LastEdited' => 'DBDatetime',
+            'MetaDescription' => 'Text',
+        ], $specs);
+
+        $specs = $method->invokeArgs($indexCreator, ['members']);
+        $this->assertEquals([
+            'FirstName' => 'Varchar',
+            'Surname' => 'Varchar',
+            'Email' => 'Varchar',
+        ], $specs);
+
+        $specs = $method->invokeArgs($indexCreator, ['flickrphotos']);
+        $this->assertEquals([
+            'Title' => 'Varchar',
+            'Description' => 'HTMLText',
+            'Aperture' => 'Float',
+            'ShutterSpeed' => 'Varchar',
+            'ISO' => 'Int',
+        ], $specs);
     }
 }
