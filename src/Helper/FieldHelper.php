955a928c3a0fd09164d9c9c3b139112f64153e02
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-10-25 22:12:20 +0000

    WIP: Dealing with MVA

--- src/Helper/FieldHelper.php
+++ src/Helper/FieldHelper.php
@@ -9,6 +9,7 @@
 
 namespace Suilven\FreeTextSearch\Helper;
 
+use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\DataObjectSchema;
 use Suilven\FreeTextSearch\Index;
 use Suilven\FreeTextSearch\Indexes;
@@ -24,13 +25,75 @@ class FieldHelper
      */
     public function getFieldValueCorrectlyTyped($index, $fieldName, $fieldValue)
     {
-        $singleton = \singleton((string)($index->getClass()));
+        $tokens = $index->getTokens();
+        $hasManyFields = $index->getHasManyFields();
+        $hasManyKeys = array_keys($hasManyFields);
 
-        $helper = new IndexingHelper();
-        $fields = $helper->getFields($index->getName());
+        $result = null;
+        if (in_array($fieldName, $tokens)) {
+            $result = $this->getSingleValueAttributeCorrectlyTyped($index, $fieldName, $fieldValue);
+        }
+        // @todo Has one
+        if (in_array($fieldName, $hasManyKeys)) {
+            $details = $hasManyFields[$fieldName];
+            $result = $this->getFieldValueCorrectlyTypedFor(
+                $details['class'],
+                $details['field'],
+                $fieldValue
+            );
 
+            $singleton = \singleton($details['class']);
+            $objInContext = $singleton->get()->filter($details['field'], $fieldValue)->first();
+
+            $result = [$objInContext->ID];
+        }
+
+        /*
+         * ===============================Array
+(
+    [q] => fish
+    [Tags] => Salton
+)
+Array
+(
+    [Tags] => Array
+        (
+            [relationship] => FlickrTags
+            [field] => RawValue
+            [class] => Suilven\Flickr\Model\Flickr\FlickrTag
+        )
+
+)
+
+         */
+
+        return $result;
+    }
+
+
+
+    /**
+     * @param Index $index
+     * @param $fieldName
+     * @param $fieldValue
+     * @return int
+     */
+    public function getSingleValueAttributeCorrectlyTyped($index, $fieldName, $fieldValue)
+    {
+        return $this->getFieldValueCorrectlyTypedFor($index->getClass(), $fieldName, $fieldValue);
+    }
+
+
+    /**
+     * @param string $clazz
+     * @param string $fieldName
+     * @param $fieldValue
+     * @return float|int|string
+     */
+    private function getFieldValueCorrectlyTypedFor($clazz, $fieldName, $fieldValue) {
+        $singleton = \singleton($clazz);
         $schema = $singleton->getSchema();
-        $specs = $schema->fieldSpecs((string) $index->getClass(), DataObjectSchema::INCLUDE_CLASS);
+        $specs = $schema->fieldSpecs($clazz, DataObjectSchema::INCLUDE_CLASS);
 
         $type = $specs[$fieldName];
         $type = explode('.', $type)[1];
@@ -47,6 +110,5 @@ class FieldHelper
         }
 
         return $value;
-
     }
 }
